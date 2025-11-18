cb86640445522a77577470c52df280715d29518f
rhodemilk <rsanc266@fiu.edu>
2025-11-18 17:33:43 -0500

    blah

--- witcon-frontend/src/pages/teams.tsx
+++ witcon-frontend/src/pages/teams.tsx
@@ -7,8 +7,17 @@ interface Team {
     name: string;
     position?: string;
 }
-
-const teams: Team[] = [
+const Executive: Team[] = [
+    {
+        name: "Margarita Gutierrez",
+        position: "President"
+    },
+    {
+        name: "Amber Garcia",
+        position: "Vice-President"
+    }
+]
+const industryRelations: Team[] = [
     {
         name: "Devin Diaz",
         position: "Industry Relations Chair"
@@ -25,19 +34,44 @@ const teams: Team[] = [
         name: "Victoria Hernandez",
         position: "Industry Relations Lead"
     },
-
-
+// make a function for ir, witcon, and then in the export
 ]
+
+
 export default function Teams(){
     return (
         <section className = 'w-full pt-6 pb-10'>
         <Header className='py-6'>Teams</Header>
-        <Title className="text-3xl font-semibold text-center mb-4">Industry Relations</Title>
-        <div className="w-full rounded-2xl bg-[color:var(--color-secondary-mint)]/100 p-66 md:p-8 shadow-md ">
-        
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-y-6 gap-x-6 place-items-center">
 
-    {teams.map((team, index) => (
+        <div className="flex flex-col md:flex-row gap-6 justify-center items-start">
+        <div className="w-full rounded-2xl bg-[color:var(--color-secondary-mint)]/100 py-6 md:p-8 shadow-md flex-row">
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-y-6 gap-x-6 place-items-center">
+        <Title className="text-3xl font-semibold text-center mb-4">Executive</Title> 
+        {/* make title on top? */}
+        {Executive.map((team, index)=>(
+        <div key={index} className="flex flex-col items-center text-cente hover:scale-105 transition-transform duration-300">
+          {/* Circle for photo */}
+          {/* make a link for everyones linkedin when you click?? */}
+          <div className="w-24 h-24 rounded-full bg-neutral-200 overflow-hidden mb-3  ">
+            {/* If you have images: */}
+            {/* <img src={team.image} alt={team.name} className="w-full h-full object-cover" /> */}
+            
+          </div>
+  
+          {/* Name + position */}
+          <Text className="font-medium leading-tight">{team.name || 'First Last Name'}</Text>
+          <Text className="text-sm opacity-80 leading-tight">{team.position || 'Position'}</Text>
+        </div>
+    ))}
+        </div>
+        </div>
+        <div className="w-full rounded-2xl bg-[color:var(--color-secondary-mint)]/100 py-6 md:p-8 shadow-md flex-row">
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-y-6 gap-x-6 place-items-center">
+{/* //make another div for each row
+//make another grid for each team */}
+
+<Title className="text-3xl font-semibold text-center mb-4">Industry Relations</Title> 
+    {industryRelations.map((team, index) => (
       <div key={index} className="flex flex-col items-center text-cente hover:scale-105 transition-transform duration-300
       ">
         {/* Circle for photo */}
@@ -47,14 +81,16 @@ export default function Teams(){
           {/* <img src={team.image} alt={team.name} className="w-full h-full object-cover" /> */}
         </div>
 
+
         {/* Name + position */}
         <Text className="font-medium leading-tight">{team.name || 'First Last Name'}</Text>
         <Text className="text-sm opacity-80 leading-tight">{team.position || 'Position'}</Text>
       </div>
     ))}
   </div>
+  </div>
         </div>
-
+        
 
 
 
