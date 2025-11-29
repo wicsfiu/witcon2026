7e689ea514f024e1c7d277055d7d1d9c73e2939c
Fer Pacheco <fernipm@gmail.com>
2025-11-29 00:25:03 -0500

    fix: Added some basic styling to the navbar

--- witcon-frontend/src/App.tsx
+++ witcon-frontend/src/App.tsx
@@ -4,21 +4,24 @@ import Register from "./pages/Register";
 import Profile from "./pages/Profile";
 import Login from "./pages/Login";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import "./App.css";
 
 export default function App() {
   return (
     <Router>
-      <Navbar />
-      <main className="p-6 max-w-7xl mx-auto w-full">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </main>
+      <div className="flex flex-col min-h-screen bg-primary-background">
+        <Navbar />
+        <main className="flex-grow p-6 max-w-7xl mx-auto w-full">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </Router>
   );
 }
-
